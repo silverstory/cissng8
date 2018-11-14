358f0784d5017f4c5f7c1023047b97a7c474e4bb
eprelofcdesktop <eprelofcdesktop@am.com>
2018-11-14 16:17:29 +0800

    Fixed no cisstoken returned on response. Used randomatic package for 8-digit code.

--- server/services/shortid-generator.js
+++ server/services/shortid-generator.js
@@ -1,4 +1,5 @@
-const shortid = require('shortid');
+// const shortid = require('shortid');
+const randomize = require('randomatic');
 const Profile = require('../models/profile.model');
 
 module.exports.CreateShortId = async () => {
@@ -8,17 +9,18 @@ module.exports.CreateShortId = async () => {
     let existing = true;
     let tries = 0;
 
-    shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
+    // shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
     //test if generated id is existing
-    //will end if no entry found or if 10 tries is consumed and still random short id exists
-    while(existing && tries <= 10) {
+    //will end if no entry found or if 100 tries is consumed and still random short id exists
+    while(existing && tries <= 100) {
 
       //generate a random short id
-      newShortId = await shortid.generate();
+      // newShortId = await shortid.generate();
+      newShortId = await randomize('0', 8);
       tries++;
 
-      const profile = await Profile.findOne({
+      const profile = await Profile.Profile.findOne({
         cisstoken: newShortId
       });
 
@@ -27,13 +29,15 @@ module.exports.CreateShortId = async () => {
       }
     }
 
-    //fail if 10 tries consumed
-    if(tries == 10) {
+    //fail if 100 tries consumed
+    if(tries == 100) {
       reject('Short Id generation failed')
     }
     else { //succeed otherwise and return new random short id
       ShortId = newShortId;
     }
+
+    console.log(ShortId);
 
     return ShortId;
 
