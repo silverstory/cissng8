bf6815a36c2c2e807654db8596e365cac460be29
Eprel Acer ICTO <apr.medina@gmail.com>
2020-01-17 10:04:14 +0800

    Added notification model on angular

---
+++ src/app/notification.ts
@@ -0,0 +1,28 @@
+export interface Notification {
+  _id: string;
+  distinction: String;
+  usertype: String;
+  username: String;
+  number: String;
+  message: String;
+  datetimesent: Date;
+}
+
+export class NotificationObj implements Notification {
+  _id: string;
+  distinction: String;
+  usertype: String;
+  username: String;
+  number: String;
+  message: String;
+  datetimesent: Date;
+
+  constructor(item?: Notification) {
+      if (item !== undefined) {
+          // tslint:disable-next-line:forin
+          for (const key in item) {
+              try { this[key] = item[key]; } catch (e) { }
+          }
+      }
+  }
+}
